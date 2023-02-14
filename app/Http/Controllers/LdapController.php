1609c0d18164ed01aeb15e922d608e5de8f53a00
hirosha <malithhirosha@gmail.com>
2023-02-14 13:29:15 +0530

    LDAP Now works

---
+++ app/Http/Controllers/LdapController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Artisan;
+
+use Illuminate\Http\Request;
+
+class LdapController extends Controller
+{
+    //
+
+
+
+    public function TestLdapData(){
+        // $val = config('ldap.host'). ':'. config('ldap.port'). '/'. config('');
+        // return "tetst";
+        // $Host = config('ldap.connections.default.hosts');
+        // $userName = config('ldap.connections.default.username');
+        // $DN = config('ldap.connections.default.base_dn');
+
+        // return view('welcome');
+
+
+
+        // $LdapTest = Artisan::call('ldap:test');
+
+
+
+
+
+        // $jsonResponse = Artisan::call('ldap:test', [], '--format=json');
+        // return redirect('/'); // Redirect to home page or any other page after executing the command.
+        //    if($c == 'yes'){
+        //         return "LDAP connection successful";
+        //    }else{
+        //         return "LDAP connection failed";
+        //    }
+
+
+    }
+
+
+
+
+
+
+}
