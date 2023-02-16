d34e9a66a058cffafd4eb30fc944d235bf805706
hirosha <malithhirosha@gmail.com>
2023-02-16 09:43:14 +0530

    ldapDone

--- app/Http/Controllers/LdapController.php
+++ app/Http/Controllers/LdapController.php
@@ -18,18 +18,18 @@ class LdapController extends Controller
 
     public function updateSettings(Request $request)
     {
-        Config::set('ldap.hosts', $request->input('hosts'));
-        Config::set('ldap.username', $request->input('username'));
-        Config::set('ldap.password', $request->input('password'));
-        Config::set('ldap.port', $request->input('port'));
-        Config::set('ldap.base_dn', $request->input('base_dn'));
+        // Config::set('ldap.hosts', $request->input('hosts'));
+        // Config::set('ldap.username', $request->input('username'));
+        // Config::set('ldap.password', $request->input('password'));
+        // Config::set('ldap.port', $request->input('port'));
+        // Config::set('ldap.base_dn', $request->input('base_dn'));
 
 
-        if ($request->has('password')) {
-            Config::set('ldap.password', $request->input('password'));
-        }
+        // if ($request->has('password')) {
+        //     Config::set('ldap.password', $request->input('password'));
+        // }
         // Redirect to the previous page with a success message
-        return back()->with('success', 'LDAP settings updated successfully');
+        // return back()->with('success', 'LDAP settings updated successfully');
 
 
 
@@ -74,48 +74,39 @@ class LdapController extends Controller
         // $request = Request::capture();
 
 
-        $serverURL = $request->input('serverURL');
-        $userField = $request->input('userField');
-        $DN = $request->input('DN');
-        $LoginID = $request->input('LoginID');
-        $password = $request->input('password');
+        // $serverURL = $request->input('serverURL');
+        // $userField = $request->input('userField');
+        // $DN = $request->input('DN');
+        // $LoginID = $request->input('LoginID');
+        // $password = $request->input('password');
 
 
     //    $file = File::put(public_path('../config\data.ini'), "[LDAP]\nServeURL => $serverURL\nuserField => $userField\n => $LoginID\npassword => $password");
         //with out DN
-        $file = File::put(public_path('../config\data.ini'), "[LDAP]\nServeURL = $serverURL\nuserField = $userField\nLoginID = $LoginID\npassword = $password");
+
+        // $file = File::put(public_path('../config\data.ini'), "[LDAP]\nServeURL = $serverURL\nuserField = $userField\nLoginID = $LoginID\npassword = $password");
+
         // return redirect()->back();
 
         // Specify the path of the .ini file
-        $path = '../config\data.ini';
+
+        // $path = '../config\data.ini';
 
         // Read the contents of the .ini file and parse it
-        $config = parse_ini_file($path, true);
+
+        // $config = parse_ini_file($path, true);
 
         // Access the values from the parsed .ini file
-        $value = $config['LDAP']['ServeURL'];
-        $value2 = $config['LDAP']['userField'];
+
+        // $value = $config['LDAP']['ServeURL'];
+        // $value2 = $config['LDAP']['userField'];
         // $value3 = $config['LDAP']['userField'];
-        $value4 = $config['LDAP']['LoginID'];
-        $value5 = $config['LDAP']['password'];
+        // $value4 = $config['LDAP']['LoginID'];
+        // $value5 = $config['LDAP']['password'];
 
 
 
-        echo '<br>URL '.  $value.'<br>USER '. $value2.'<br>UserName '. $value4.'<br>Password '. $value5."\n<br>";
-
-
-
-
-
-
-
-
-
-
-        return view('welcome')->with('value' , $value);
-
-
-        // echo $file;
+        // echo '<br>URL '.  $value.'<br>USER '. $value2.'<br>UserName '. $value4.'<br>Password '. $value5."\n<br>";
 
 
 
@@ -126,25 +117,8 @@ class LdapController extends Controller
 
 
 
-
-        // $array = [1, 2, 3, 4, 5];
-        // $data = implode(',', $array); // convert array to comma-separated string
-
-        // // save data to file
-        // file_put_contents('data.txt', $data);
+        // return view('welcome')->with('value' , $value);
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
     public function TestLdapData(){
          $testPass ="Connection Test Passed";
@@ -191,12 +165,6 @@ class LdapController extends Controller
             ]);
         }
 
-
-
-
-
-        //return view("connection", with(["va"=>$t]));
-        // return view("connection")->with('t',$t);
 
 
     }
